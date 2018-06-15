e7812ee8ada6453c1e04150e065c4b55e7b61f9e
Chris DeBracy <chrisdebracy@precocityllc.com>
2018-06-15 15:45:45 -0500

    Updates to use GOOGLE_CLOUD_PROJECT instead of DEVSHELL_PROJECT_ID for consistency.

--- scripts/02_variableSubstitution.sh
+++ scripts/02_variableSubstitution.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 find . -type f -exec sed -i "s/{USER}/$USER/g" {} +
-find . -type f -exec sed -i "s/{DEVSHELL_PROJECT_ID}/$DEVSHELL_PROJECT_ID/g" {} +
+find . -type f -exec sed -i "s/{GOOGLE_CLOUD_PROJECT}/$GOOGLE_CLOUD_PROJECT/g" {} +
 chmod +x ~/gcp-retail-workshop-2018/airflow/ansible/airflow/ansible_install.sh
 
