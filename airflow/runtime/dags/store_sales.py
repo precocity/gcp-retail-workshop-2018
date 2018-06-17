92ec05971e836fcd228e4bb4ea1fc8239d383a5e
Chris DeBracy <chrisdebracy@precocityllc.com>
2018-06-17 14:51:26 -0500

    Update to use bash operator for BQ temporarily.

--- airflow/runtime/dags/store_sales.py
+++ airflow/runtime/dags/store_sales.py
@@ -2,7 +2,8 @@ import os
 from datetime import datetime, timedelta
 
 from airflow import DAG
-from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+# from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.operators.bash_operator import BashOperator
 
 airflow_home = os.environ.get('AIRFLOW_HOME')
 
@@ -15,15 +16,18 @@ default_args = {
 }
 
 dag = DAG('bq_stores_sales_processing',
-          schedule_interval='(*/5 * * * *',
+          schedule_interval=None,
           catchup=False,
           default_args=default_args)
 
-bq_store_sales_task = BigQueryOperator(
-    task_id='bq_store_sales',
-    bql='scripts/store_sales.sql',
-    bigquery_conn_id='google_cloud_default',
-    use_legacy_sql=False,
-    delegate_to='airflow@{GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com'
-)
+bq_store_sales_task = BashOperator(task_id='bq_store_sales_bash',
+                                   bash_command='bq query --use_legacy_sql=False `cat /etc/airflow/scripts/store_sales.sql`',
+                                   dag=dag)
 
+# bq_store_sales_task = BigQueryOperator(
+#     task_id='bq_store_sales',
+#     bql=airflow_home + '/scripts/store_sales.sql',
+#     bigquery_conn_id='google_cloud_default',
+#     use_legacy_sql=False,
+#     delegate_to='airflow@{GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com',
+#     dag=dag)
