b78bf4e4dfdaef52eeac0e20148bf95da001d02f
Chris DeBracy <chrisdebracy@precocityllc.com>
2018-06-20 09:32:10 -0500

    Update to API's init script. Small fix to dag to make it less likely it will fail after editing during the labs.

--- airflow/runtime/dags/store_sales.py
+++ airflow/runtime/dags/store_sales.py
@@ -24,7 +24,7 @@ bq_store_sales_task = BashOperator(task_id='bq_store_sales_bash',
                                   bash_command='bq query --use_legacy_sql=False < ' + airflow_home + '/scripts/store_sales.sql ',
                                   dag=dag)
 
-# bq_store_sales_task = BigQueryOperator(
+#bq_store_sales_task = BigQueryOperator(
 #      task_id='bq_store_sales',
 #      bql="INSERT `retail_demo_warehouse.store_sales` (store_id, store_name, lat_long, transaction_date, total_sales, updated_timestamp) SELECT store.store_id, store.store_name, CONCAT(CAST(store.latitude AS STRING), CONCAT(',', CAST(store.longitude AS STRING))) AS lat_long, sales.transaction_date, sales.store_sales AS total_sales, CURRENT_TIMESTAMP() as updated_timestamp FROM ( SELECT transaction_date, CAST(store_id AS INT64) AS store_id, ROUND(SUM((SELECT SUM(item_price_each * quantity) FROM UNNEST(lineitems))),2) AS store_sales FROM `retail_demo_warehouse.sales_events` GROUP BY transaction_date, store_id ) sales JOIN `retail_demo_warehouse.store` store ON store.store_id = sales.store_id",
 #      sql=None,
